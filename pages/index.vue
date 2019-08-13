a9815380e985c02ce4c65f240c0b6e307d439fbf
jian <janine.jian.chen@gmail.com>
2019-08-13 15:25:19 +0800

    delete default layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,62 +1,35 @@
 <template>
-    <v-layout column justify-center align-center>
-        <v-flex xs12 sm8 md6>
-            <div class="text-center">
-                <logo />
-                <vuetify-logo />
-            </div>
-            <v-card>
-                <v-card-title class="headline">
-                    Welcome to the Vuetify + Nuxt.js template
-                </v-card-title>
-                <v-card-text>
-                    <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-                    <p>
-                        For more information on Vuetify, check out the <a href="https://vuetifyjs.com" target="_blank">
-                            documentation
-                        </a>.
-                    </p>
-                    <p>
-                        If you have questions, please join the official <a href="https://chat.vuetifyjs.com/" target="_blank" title="chat">
-                            discord
-                        </a>.
-                    </p>
-                    <p>
-                        Find a bug? Report it on the github <a href="https://github.com/vuetifyjs/vuetify/issues" target="_blank" title="contribute">
-                            issue board
-                        </a>.
-                    </p>
-                    <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-                    <div class="text-xs-right">
-                        <em><small>&mdash; John Leider</small></em>
-                    </div>
-                    <hr class="my-3">
-                    <a href="https://nuxtjs.org/" target="_blank">
-                        Nuxt Documentation
-                    </a>
-                    <br>
-                    <a href="https://github.com/nuxt/nuxt.js" target="_blank">
-                        Nuxt GitHub
-                    </a>
-                </v-card-text>
-                <v-card-actions>
-                    <v-spacer />
-                    <v-btn color="primary" nuxt to="/inspire">
-                        Continue
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-flex>
-    </v-layout>
+    <v-content>
+        <v-card class="" color="#E0F2F1" >
+            <v-card-text class="" style="height: 300px;">
+              <div class="headline ma-10">Software Development Engineer</div>
+              I am a Software Development Engineer of ChengDu, Here is My Official Homepage
+            </v-card-text>
+        </v-card>
+        <v-row>
+            <v-col>
+                    <v-card class="ma-1">
+                        <v-card-title class="teal lighten-5">个人站点</v-card-title>
+                        <v-card-text style="height: 300px;"></v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-card class="ma-1">
+                        <v-card-title class="teal lighten-5">开源项目</v-card-title>
+                        <v-card-text style="height: 300px;"></v-card-text>
+                    </v-card>
+                </v-col>
+        </v-row>
+    </v-content>
 </template>
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-    components: {
-        Logo,
-        VuetifyLogo
+    layout: 'homepage',
+    data: () => ({
+
+    }),
+    methods: {
+
     }
 }
 </script>
