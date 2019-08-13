b78cc0b4583d11bc24b2d1cb47049e59caf2aa48
jian <janine.jian.chen@gmail.com>
2019-08-13 14:42:13 +0800

    add homepage page

---
+++ layouts/homepage.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-app>
+        <v-toolbar color="white" fixed app>
+            <v-toolbar-title>JIAN</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items>
+                <v-btn text>GITHUB</v-btn>
+                <v-btn text>ABOUT ME</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-sheet>
+            <v-container fluid>
+                <nuxt />
+            </v-container>
+        </v-sheet>
+    </v-app>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    },
+
+}
+</script>
---
+++ pages/inspire.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-content>
+        <v-card class="" color="#E0F2F1" >
+            <v-card-text class="" style="height: 300px;">
+              <div class="headline ma-10">Software Development Engineer</div>
+              I am a Software Development Engineer of ChengDu, Here is My Official Homepage
+            </v-card-text>
+        </v-card>
+        <v-row>
+            <v-col>
+                    <v-card class="ma-1">
+                        <v-card-title class="teal lighten-5">个人站点</v-card-title>
+                        <v-card-text style="height: 300px;"></v-card-text>
+                    </v-card>
+                </v-col>
+                <v-col>
+                    <v-card class="ma-1">
+                        <v-card-title class="teal lighten-5">开源项目</v-card-title>
+                        <v-card-text style="height: 300px;"></v-card-text>
+                    </v-card>
+                </v-col>
+        </v-row>
+    </v-content>
+</template>
+<script>
+export default {
+    layout: 'homepage',
+    data: () => ({
+
+    }),
+    methods: {
+
+    }
+}
+</script>
